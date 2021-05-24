03aeda2f5770e0a8001abbad268da78306a39518
jhnpaulogrc <johnpaulogarcia03@gmail.com>
2021-05-24 23:42:17 +0800

    Program design and Naming Variables

---
+++ Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttn1 = new System.Windows.Forms.Button();
+            this.bttn2 = new System.Windows.Forms.Button();
+            this.bttn3 = new System.Windows.Forms.Button();
+            this.bttn4 = new System.Windows.Forms.Button();
+            this.bttn5 = new System.Windows.Forms.Button();
+            this.bttn6 = new System.Windows.Forms.Button();
+            this.bttn7 = new System.Windows.Forms.Button();
+            this.bttn8 = new System.Windows.Forms.Button();
+            this.bttn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bttn1
+            // 
+            this.bttn1.BackColor = System.Drawing.Color.Gray;
+            this.bttn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn1.Location = new System.Drawing.Point(39, 49);
+            this.bttn1.Name = "bttn1";
+            this.bttn1.Size = new System.Drawing.Size(75, 75);
+            this.bttn1.TabIndex = 0;
+            this.bttn1.UseVisualStyleBackColor = false;
+            this.bttn1.Click += new System.EventHandler(this.bttn1_Click);
+            // 
+            // bttn2
+            // 
+            this.bttn2.BackColor = System.Drawing.Color.Gray;
+            this.bttn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn2.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn2.Location = new System.Drawing.Point(122, 49);
+            this.bttn2.Name = "bttn2";
+            this.bttn2.Size = new System.Drawing.Size(75, 75);
+            this.bttn2.TabIndex = 1;
+            this.bttn2.UseVisualStyleBackColor = false;
+            // 
+            // bttn3
+            // 
+            this.bttn3.BackColor = System.Drawing.Color.Gray;
+            this.bttn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn3.Location = new System.Drawing.Point(204, 49);
+            this.bttn3.Name = "bttn3";
+            this.bttn3.Size = new System.Drawing.Size(75, 75);
+            this.bttn3.TabIndex = 2;
+            this.bttn3.UseVisualStyleBackColor = false;
+            // 
+            // bttn4
+            // 
+            this.bttn4.BackColor = System.Drawing.Color.Gray;
+            this.bttn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn4.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn4.Location = new System.Drawing.Point(39, 130);
+            this.bttn4.Name = "bttn4";
+            this.bttn4.Size = new System.Drawing.Size(75, 75);
+            this.bttn4.TabIndex = 3;
+            this.bttn4.UseVisualStyleBackColor = false;
+            // 
+            // bttn5
+            // 
+            this.bttn5.BackColor = System.Drawing.Color.Gray;
+            this.bttn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn5.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn5.Location = new System.Drawing.Point(122, 130);
+            this.bttn5.Name = "bttn5";
+            this.bttn5.Size = new System.Drawing.Size(75, 75);
+            this.bttn5.TabIndex = 4;
+            this.bttn5.UseVisualStyleBackColor = false;
+            // 
+            // bttn6
+            // 
+            this.bttn6.BackColor = System.Drawing.Color.Gray;
+            this.bttn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn6.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn6.Location = new System.Drawing.Point(204, 130);
+            this.bttn6.Name = "bttn6";
+            this.bttn6.Size = new System.Drawing.Size(75, 75);
+            this.bttn6.TabIndex = 5;
+            this.bttn6.UseVisualStyleBackColor = false;
+            // 
+            // bttn7
+            // 
+            this.bttn7.BackColor = System.Drawing.Color.Gray;
+            this.bttn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn7.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn7.Location = new System.Drawing.Point(39, 216);
+            this.bttn7.Name = "bttn7";
+            this.bttn7.Size = new System.Drawing.Size(75, 75);
+            this.bttn7.TabIndex = 6;
+            this.bttn7.UseVisualStyleBackColor = false;
+            // 
+            // bttn8
+            // 
+            this.bttn8.BackColor = System.Drawing.Color.Gray;
+            this.bttn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn8.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn8.Location = new System.Drawing.Point(122, 216);
+            this.bttn8.Name = "bttn8";
+            this.bttn8.Size = new System.Drawing.Size(75, 75);
+            this.bttn8.TabIndex = 7;
+            this.bttn8.UseVisualStyleBackColor = false;
+            // 
+            // bttn9
+            // 
+            this.bttn9.BackColor = System.Drawing.Color.Gray;
+            this.bttn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn9.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bttn9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttn9.Location = new System.Drawing.Point(204, 216);
+            this.bttn9.Name = "bttn9";
+            this.bttn9.Size = new System.Drawing.Size(75, 75);
+            this.bttn9.TabIndex = 8;
+            this.bttn9.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(321, 335);
+            this.Controls.Add(this.bttn9);
+            this.Controls.Add(this.bttn8);
+            this.Controls.Add(this.bttn7);
+            this.Controls.Add(this.bttn6);
+            this.Controls.Add(this.bttn5);
+            this.Controls.Add(this.bttn4);
+            this.Controls.Add(this.bttn3);
+            this.Controls.Add(this.bttn2);
+            this.Controls.Add(this.bttn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttn1;
+        private System.Windows.Forms.Button bttn2;
+        private System.Windows.Forms.Button bttn3;
+        private System.Windows.Forms.Button bttn4;
+        private System.Windows.Forms.Button bttn5;
+        private System.Windows.Forms.Button bttn6;
+        private System.Windows.Forms.Button bttn7;
+        private System.Windows.Forms.Button bttn8;
+        private System.Windows.Forms.Button bttn9;
+    }
+}
+
