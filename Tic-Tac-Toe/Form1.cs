a11efbd9d3f72602ee0325d9c9f28d1a53a09cb5
jhnpaulogrc <johnpaulogarcia03@gmail.com>
2021-07-03 01:21:11 +0800

    Code Update

--- Tic-Tac-Toe/Form1.cs
+++ Tic-Tac-Toe/Form1.cs
@@ -24,7 +24,7 @@ namespace Tic_Tac_Toe
         }
 
         // Code for the input of the players
-        private void Click_Input(object sender, EventArgs e)
+        public void Click_Input(object sender, EventArgs e)
         {
             Button Input = (Button)sender;
 
@@ -45,34 +45,41 @@ namespace Tic_Tac_Toe
                ((bttn7.Text == "X") && (bttn8.Text == "X") && (bttn9.Text == "X")))
             {
                 MessageBox.Show("Player 1 Wins!");
+
+                gameRestart();
             }
             else if (((bttn1.Text == "O") && (bttn2.Text == "O") && (bttn3.Text == "O")) ||
                ((bttn4.Text == "O") && (bttn5.Text == "O") && (bttn6.Text == "O")) ||
                ((bttn7.Text == "O") && (bttn8.Text == "O") && (bttn9.Text == "O")))
             {
                 MessageBox.Show("Player 2 Wins!");
+                gameRestart();
             }
             else if (((bttn1.Text == "X") && (bttn4.Text == "X") && (bttn7.Text == "X")) ||
               ((bttn2.Text == "X") && (bttn5.Text == "X") && (bttn8.Text == "X")) ||
               ((bttn3.Text == "X") && (bttn6.Text == "X") && (bttn9.Text == "X")))
             {
                 MessageBox.Show("Player 1 Wins!");
+                gameRestart();
             }
             else if (((bttn1.Text == "O") && (bttn4.Text == "O") && (bttn7.Text == "O")) ||
                ((bttn2.Text == "O") && (bttn5.Text == "O") && (bttn8.Text == "O")) ||
                ((bttn3.Text == "O") && (bttn6.Text == "O") && (bttn9.Text == "O")))
             {
                 MessageBox.Show("Player 2 Wins!");
+                gameRestart();
             }
             else if (((bttn1.Text == "X") && (bttn5.Text == "X") && (bttn9.Text == "X")) ||
              ((bttn3.Text == "X") && (bttn5.Text == "X") && (bttn7.Text == "X")))
             {
                 MessageBox.Show("Player 1 Wins!");
+                gameRestart();
             }
             else if (((bttn1.Text == "O") && (bttn5.Text == "O") && (bttn9.Text == "O")) ||
              ((bttn3.Text == "O") && (bttn5.Text == "O") && (bttn7.Text == "O")))
             {
                 MessageBox.Show("Player 2 Wins!");
+                gameRestart();
             }
 
          // Code if ever the match is draw
@@ -87,7 +94,30 @@ namespace Tic_Tac_Toe
                ((bttn9.Text == "X") || (bttn9.Text == "O") && (bttn9.Enabled == false)))
             {
                 MessageBox.Show("Draw!");
+                gameRestart();
             }
+        }
+        // Code if ever the match is already finish
+        private void gameRestart()
+        {
+            bttn1.Text = "";
+            bttn2.Text = "";
+            bttn3.Text = "";
+            bttn4.Text = "";
+            bttn5.Text = "";
+            bttn6.Text = "";
+            bttn7.Text = "";
+            bttn8.Text = "";
+            bttn9.Text = "";
+            bttn1.Enabled = true;
+            bttn2.Enabled = true;
+            bttn3.Enabled = true;
+            bttn4.Enabled = true;
+            bttn5.Enabled = true;
+            bttn6.Enabled = true;
+            bttn7.Enabled = true;
+            bttn8.Enabled = true;
+            bttn9.Enabled = true;
         }
     }
 }
